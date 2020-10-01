ca3c2003fc2ff6b8c576f339491e42f608cfd2c1
Randy <rlthomasccv@gmail.com>
2020-10-01 13:08:55 -0700

    Heroku build 1.0

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
@@ -11,11 +10,6 @@ const app = express();
 app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'zara-mila',
-  resave: false,
-  saveUninitialized: false,
-}));
 
 
 app.listen(port, () => {
