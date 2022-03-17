d3809039e3932f96c2f4be9c2e547266aa568acd
Randy Thomas <randythomas@Randys-MacBook-Pro.local>
2022-03-17 14:39:52 -0700

    added luke

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
-import Modal from 'react-awesome-modal';
-import axios from 'axios';
-import {
-  Redirect, Route, HashRouter, Link,
-} from 'react-router-dom';
 import Routes from './Routes';
 
 class App extends Component {
